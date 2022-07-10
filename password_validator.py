b3f734fd30ac0952171feb40f71192250562bbb2
tbarlev <31614673+tbarlev@users.noreply.github.com>
2022-07-10 17:42:42 +0300

    Add files via upload

---
+++ password_validator.py
@@ -0,0 +1,54 @@
+# This script validates a given password
+
+#Import color library to print output in color
+import colorama
+from colorama import Fore
+import sys
+
+def validate(passwd):
+    """
+    Validate password - length, charachters
+    :param passwd:
+    :return: 0 if password is valid, 1 if not valid + reason
+    """
+    global reason
+
+    if len(passwd) < 10:
+        print(len(passwd))
+        reason = "Password should be at least 10 charachters"
+        return 1
+
+    if not any(char.isdigit() for char in passwd):
+        reason = "Password should have at least one numeric charachter"
+        return 1
+
+    if not any(char.isupper() for char in passwd):
+        reason = "Password should have at least one uppercase letter"
+        return 1
+
+    if not any(char.islower() for char in passwd):
+        reason = "Password should have at least one lowercase letter"
+        return 1
+
+    return 0
+
+
+
+passwd = sys.argv[1]
+
+if validate(passwd):
+        print(Fore.RED + "password is not valid, " + reason)
+else:
+        print(Fore.GREEN + "Password is valid")
+
+
+
+
+
+
+
+
+
+
+
+
